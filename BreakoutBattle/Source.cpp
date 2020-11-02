d0c9d967328d260ff36560203195aaea4972d352
samsonhunk <legomansam@gmail.com>
2020-11-02 23:42:29 +0000

    Socket fix

--- BreakoutBattle/Source.cpp
+++ BreakoutBattle/Source.cpp
@@ -4,9 +4,32 @@
 #include <iostream>
 #include <string>
 #include "InputManager.h"
+#include <thread>
+#include <mutex>
 
 #define MAXPLAYERS 2
 #define MAXBALLS 2
+
+///////////////////////////////////////////////
+//networking objects
+sf::IpAddress otherIp;
+unsigned short otherPort;
+unsigned short port;
+//packet definition
+struct PacketType
+{
+	unsigned int packetNum = 0; //packet iterator to keep track of the most up to date packet
+	float playerPos = 0; //current x pos of the other player
+	int playerDirection = 0;
+};
+
+PacketType lastPacket;
+PacketType outPacket;
+bool newPacketFlag = false;
+bool foundPlayer = false;
+
+std::mutex networkLock;
+////////////////////////////////////////////////
 
 ///////////////////////////////////////////
 //game objects
@@ -16,6 +39,7 @@ struct Player
 	{
 		playerShape.setSize(sf::Vector2f(60, 10));
 	}
+
 	sf::RectangleShape playerShape;
 	sf::Vector2f playerPos;
 	int playerScore = 0;
@@ -55,7 +79,7 @@ struct GameState
 //function forward declarations
 void update(float dt, GameState* gameState, InputManager* inputManager, sf::RenderWindow* window);
 void render(float dt, GameState* gameState, InputManager* inputManager, sf::RenderTexture* renderTex, sf::RenderWindow* window);
-void checkPackets(sf::UdpSocket* socket, GameState* gameState);
+void checkPackets(sf::UdpSocket* socket, GameState* gameState, sf::RenderWindow* renderWindow);
 void sendPackets(sf::UdpSocket* socket, GameState* gameState);
 
 ///////////////////////////////////////////////////////////////////////  vector maths
@@ -79,51 +103,49 @@ float Dot(sf::Vector2f v1, sf::Vector2f v2)
 int main()
 {
 	sf::UdpSocket socket;
-	unsigned short port = 5400;
-	std::string otherIp = "";
-
+	socket.setBlocking(true);
+	
 	{//bind UDP socket
 		int answer;
 		//initial game setup
-		std::cout << "1 Connect ip, 2 Connet localhost" << std::endl;
-		std::cin >> answer;
-		switch (answer)
-		{
-		case 1:
-			if (socket.bind(port, sf::IpAddress::getPublicAddress()) != socket.Done)
-			{
-				return 2; //exit game if unable to bind the socket
-			}
-			else
-			{
-				std::cout << "Bound public socket on port " << std::to_string(port) << std::endl;
-			}
-			break;
-		case 2:
-			if (socket.bind(port, sf::IpAddress::getLocalAddress()) != socket.Done)
-			{
-				return 2; //exit game if unable to bind the socket
-			}
-			else
-			{
-				std::cout << "Bound local socket on port " << std::to_string(port) << std::endl;
-			}
-			break;
-		default:
-			return 3;
-			break;
-		}
-
+	
 		std::cout << "1 Host game, 2 Join game" << std::endl;
 		std::cin >> answer;
+
+		if (answer == 1)
+		{
+			port = 5400;
+			otherPort = 5410;
+		}
+		else
+		{
+			port = 5410;
+			otherPort = 5400;
+		}
+
+		if (socket.bind(port, sf::IpAddress::LocalHost) != socket.Done)
+		{
+			return 2; //exit game if unable to bind the socket
+		}
+		else
+		{
+			std::cout << "Bound socket on port " << std::to_string(port) << std::endl;
+		}
+
+		std::string entry;
 		switch (answer)
 		{
 		case 1:
 			std::cout << "Waiting for player to join...";
 			break;
 		case 2:
-			std::cout << "Enter host ip: " << std::endl;
-			std::cin >> otherIp;
+			do
+			{
+				std::cout << "Enter host ip: " << std::endl;
+				std::cin >> otherIp;
+			} while (otherIp == sf::IpAddress::None);
+			foundPlayer = true;
+			break;
 		default:
 			return 3;
 			break;
@@ -148,6 +170,9 @@ int main()
 	gameState.players[0].playerPos = sf::Vector2f(0, 790);
 	gameState.balls[0].lastHitBy = 0;
 	gameState.balls[1].lastHitBy = 1;
+
+	//create packet receipt thread
+	std::thread receiptThread(checkPackets, &socket, &gameState, &window);
 
 	while (window.isOpen())
 	{
@@ -175,13 +200,15 @@ int main()
 		clock.restart();
 
 		window.clear();
-		//checkPackets(&socket, &gameState);
+		networkLock.lock();
 		update(frameTime.asSeconds(), &gameState, &inputManager, &window);
 		render(frameTime.asSeconds(), &gameState, &inputManager, &renderTex, &window);
-		//sendPackets(&socket, &gameState);
+		networkLock.unlock();
+		sendPackets(&socket, &gameState);
 		window.display();
 	}
 
+	receiptThread.join();
 	return 0;
 }
 
@@ -191,49 +218,64 @@ void update(float dt, GameState* gameState, InputManager* inputManager, sf::Rend
 	if (inputManager->GetKey(sf::Keyboard::Left))
 	{
 		gameState->players[0].playerPos.x -= gameState->players[0].playerSpeed * dt;
+		outPacket.playerDirection = 1;
 	}
 	else if (inputManager->GetKey(sf::Keyboard::Right))
 	{
 		gameState->players[0].playerPos.x += gameState->players[0].playerSpeed * dt;
+		outPacket.playerDirection = 2;
 	}
-	else if (inputManager->GetKey(sf::Keyboard::Space))
+	else
+	{
+		outPacket.playerDirection = 0;
+	}
+
+	if (inputManager->GetKey(sf::Keyboard::Space))
 	{//press space to fire a ball
-		for (int it = 0; it < 2; it++)
+		for (int it = 0; it < MAXBALLS; it++)
 		{
-			if (gameState->balls[it].lastHitBy == 1 && !gameState->balls[it].isShot)
+			if (gameState->balls[it].lastHitBy == 0 && !gameState->balls[it].isShot)
 			{
 				gameState->balls[it].isShot = true;
 				//angle the ball if the player is moving while shooting the ball
 				if (inputManager->GetKey(sf::Keyboard::Left))
 				{
-					gameState->balls[it].ballDir = sf::Vector2f(-1.f, -0.5f);
+					gameState->balls[it].ballDir = sf::Vector2f(-.5f, -1.f);
 				}
 				else if (inputManager->GetKey(sf::Keyboard::Right))
 				{
-					gameState->balls[it].ballDir = sf::Vector2f(-1.f, 0.5f);
+					gameState->balls[it].ballDir = sf::Vector2f(.5f, -1.f);
 				}
 				else
 				{
-					gameState->balls[it].ballDir = sf::Vector2f(-1.f, 0.f);
+					gameState->balls[it].ballDir = sf::Vector2f(0.f, -1.f);
 				}
-				gameState->balls[it].lastHitBy = 1;
+				gameState->balls[it].lastHitBy = 0;
 				break;
 			}
 		}
 	}
 
 	//update the ball positions
-	for (int i = 0; i < 2; ++i)
+	for (int it = 0; it < MAXBALLS; ++it)
 	{
 		//if the ball has already been shot then move it
-		int debug = i;
-		if (gameState->balls[i].isShot)
+		if (gameState->balls[it].isShot)
 		{
-			gameState->balls[i].ballPos += gameState->balls[i].ballDir * (gameState->balls[i].ballSpeed * dt);
+			gameState->balls[it].ballPos += gameState->balls[it].ballDir * (gameState->balls[it].ballSpeed * dt);
 		}
 		else //else, keep it connected to the player until they release it
 		{
-			gameState->balls[i].ballPos = gameState->players[gameState->balls[i].lastHitBy].playerPos - sf::Vector2f(-25, 10);
+			float offset;
+			if (gameState->balls[it].lastHitBy == 0)
+			{
+				offset = 10.f;
+			}
+			else
+			{
+				offset = -10.f;
+			}
+			gameState->balls[it].ballPos = gameState->players[gameState->balls[it].lastHitBy].playerPos - sf::Vector2f(-25.f, offset);
 		}
 	}
 }
@@ -243,19 +285,18 @@ void render(float dt, GameState* gameState, InputManager* inputManager, sf::Rend
 	//render all the objects onto the screen
 
 
-	for (int it = 0; it < 2; it++)
+	for (int it = 0; it < MAXPLAYERS; it++)
 	{
-		gameState->players[it].playerShape.setPosition(gameState->players->playerPos);
+		gameState->players[it].playerShape.setFillColor(sf::Color::Green);
+		gameState->players[it].playerShape.setPosition(gameState->players[it].playerPos);
 		window->draw(gameState->players[it].playerShape);
 	}
-
 
 	//TODO: render the level
 
 
 	//render the balls
-
-	for (int it = 0; it < 2; it++)
+	for (int it = 0; it < MAXPLAYERS; it++)
 	{
 		gameState->balls[it].ballShape.setPosition(gameState->balls[it].ballPos);
 		window->draw(gameState->balls[it].ballShape);
@@ -263,12 +304,55 @@ void render(float dt, GameState* gameState, InputManager* inputManager, sf::Rend
 
 }
 
-void checkPackets(sf::UdpSocket* socket, GameState* gameState)
+
+
+void checkPackets(sf::UdpSocket* socket, GameState* gameState, sf::RenderWindow* renderWindow)
 {
-	//check for any new packets and apply their data to the gamestate
+	sf::IpAddress ipIn;
+	unsigned short portIn;
+	while (renderWindow->isOpen())
+	{
+		sf::Packet packet;
+		//check for new packets
+		if (socket->receive(packet, ipIn, portIn) != socket->Done)
+		{
+			networkLock.lock();
+			std::cout << "Error retrieving packet" << std::endl;
+			networkLock.unlock();
+		}
+		else
+		{
+			networkLock.lock();
+			std::cout << "Packet received" << std::endl;
+			foundPlayer = true;
+			//if a whole packet is received then act on the data
+			unsigned int packetNum;
+			packet >> packetNum;
+
+			//check that the new packet is more up to date than the last received packet
+			if (packetNum > lastPacket.packetNum)
+			{// if it does then push the data into the packet struct
+				lastPacket.packetNum = packetNum;
+				packet >> lastPacket.playerPos >> lastPacket.playerDirection;
+			}
+			newPacketFlag = true;
+			networkLock.unlock();
+		}
+	}
 }
 
 void sendPackets(sf::UdpSocket* socket, GameState* gameState)
 {
-	//send a packet with the most up to date information
+	if (foundPlayer)
+	{
+		sf::Packet packet;
+		outPacket.packetNum++;
+		packet << outPacket.packetNum << outPacket.playerPos << outPacket.playerDirection;
+
+		//send a packet with the most up to date information
+		if (socket->send(packet, otherIp, otherPort) != socket->Done)
+		{
+			std::cout << "Packet failed to send" << std::endl;
+		}
+	}
 }
